59f3988e487608be46213d382fdc5ae4c8e31d89
nggadev <nggadev@gmail.com>
2021-09-22 15:14:52 +0700

    update

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// eslint-disable-next-line
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
