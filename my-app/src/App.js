2f184c3f3bf2cfd1b849fcbba28c609275803b54
nggadev <nggadev@gmail.com>
2021-09-22 16:54:59 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,9 +6,18 @@ import './App.css';
 function App() {
 
   return (
-    <div>
-      <h1>Test</h1>
-      <p>test</p>
+    <div class="Parentbox">
+      <div class="Foto">
+        <img src="S1.png" />
+      </div>  
+      <div class="Deskripsi">
+        <p class="Cate">SNEAKER</p>
+        <h1 class="Title">Nike</h1>
+        <p class="Price">IDR 73.309.999</p>
+        <p class="Info">
+          One of the most recognizable shoes in the Nike collection, The Air Unknown features lightweight, visible cushioning just like the original from '88. Signature details and materials celebrate the game-changing icon.
+        </p>
+      </div>  
     </div>
   );
 }
