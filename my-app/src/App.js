4f389a9817fffc4d23f318d3ad3939e7e5081b04
muhanggaohs <angga.oktaharisetia@gmail.com>
2021-09-23 15:53:26 +0700

    split component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductReviews from './Reviews'
 
 function App() {
   return (
@@ -8,49 +9,8 @@ function App() {
       <ProductFoto />
       <ProductInfo isDiscount="yes" name="Air Jordan" category="HYPE" />
       <ProductReviews />
-
-      
     </div>
   );
-}
-
-function ProductReviews () {
-//  prepare Data dummy JSON
-const users = [
-   {
-     "id": 1,
-     "name": "Takamichi",
-     "review": "GGWP !!!!",
-     "pic": "https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-   },
-   {
-     "id": 2,
-     "name": "Draken",
-     "review": "GGWP !!!!",
-     "pic": "https://images.pexels.com/photos/3671083/pexels-photo-3671083.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-   },
-   {
-     "id": 3,
-     "name": "Mikey",
-     "review": "GGWP !!!!",
-     "pic": "https://images.pexels.com/photos/3283568/pexels-photo-3283568.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-   }
-  ];
-  const listReviews = users.map((itemReviews) =>
-      <div className="Item">
-        <img src={itemReviews.pic} />
-        <div className="User">
-          <h3>{itemReviews.name}</h3>
-          <p>{itemReviews.review}</p>
-        </div>
-      </div>
-    );
-    return (
-      <div className="Review-box">
-        <h2>Reviews</h2>
-        {listReviews}
-      </div>
-    );
 }
 
 function  ProductFoto() {
---
+++ my-app/src/Reviews.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+function Reviews () {
+    //  prepare Data dummy JSON
+    const users = [
+       {
+         "id": 1,
+         "name": "Takamichi",
+         "review": "GGWP !!!!",
+         "pic": "https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+       },
+       {
+         "id": 2,
+         "name": "Draken",
+         "review": "GGWP !!!!",
+         "pic": "https://images.pexels.com/photos/3671083/pexels-photo-3671083.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+       },
+       {
+         "id": 3,
+         "name": "Mikey",
+         "review": "GGWP !!!!",
+         "pic": "https://images.pexels.com/photos/3283568/pexels-photo-3283568.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+       }
+      ];
+      const listReviews = users.map((itemReviews) =>
+          <div className="Item">
+            <img src={itemReviews.pic} />
+            <div className="User">
+              <h3>{itemReviews.name}</h3>
+              <p>{itemReviews.review}</p>
+            </div>
+          </div>
+        );
+        return (
+          <div className="Review-box">
+            <h2>Reviews</h2>
+            {listReviews}
+          </div>
+        );
+    }
+
+export default Reviews;
