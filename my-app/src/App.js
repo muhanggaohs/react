f3950e14d07ed80218ee262ac608f049c48c86b1
muhanggaohs <angga.oktaharisetia@gmail.com>
2021-09-23 14:13:24 +0700

    conditional rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="Parentbox">
         <ProductFoto />
-        <ProductInfo title="NAME" category="LEBARAN" />
+        <ProductInfo isDiscount="coming" title="Air Jordan" category="LEBARAN" />
     </div>
   );
 }
@@ -19,21 +19,45 @@ function  ProductFoto() {
   );
 }
 
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+  if (isDiscount == "yes") {
+    return (
+      <p>Diskon 50% Off</p>
+    );
+  }
+  else if (isDiscount == "coming") {
+    return (
+      <p>Soon</p>
+    );
+  }
+  else {
+    return (
+      <p>No Discount !</p>
+    );
+  }
+}
+
 function ProductInfo(props) {
-  const { category, title } = props;
+  const { category, title, isDiscount } = props;
   return (
     <div>
       <div className="Deskripsi">
         <p className="Cate">{category}</p>
         <h1 className="Title">{title}</h1>
         <p className="Price">IDR 73.309.999</p>
+        <CheckDiscount isDiscount={isDiscount} />
         <p className="Info">
           One of the mosat recognizable shoes in the Nike collection, The Air Unknown features lightweight, visible cushioning just like the original from '88. Signature details and materials celebrate the game-changing icon.
         </p>
-        <a href="#">Add to Cart</a>
+        <a onClick={(e) => AddCart(title, e)} href="#">Add to Cart</a>
       </div>  
     </div>
   );
+}
+
+function AddCart(e){
+  return console.log("Add (" + e + ")");
 }
 
 export default App;
