8dccd15ac0714ea38e5521b2a1539183dd18109f
nggadev <nggadev@gmail.com>
2021-09-23 10:09:05 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
   return (
-    <div class="Parentbox">
-      <div class="Foto">
+    <div className="Parentbox">
+        <ProductFoto />
+        <ProductInfo title="NAME" category="LEBARAN" />
+    </div>
+  );
+}
+
+function  ProductFoto() {
+  return (
+    <div className="Foto">
         <img src="S1.png" />
-      </div>  
-      <div class="Deskripsi">
-        <p class="Cate">SNEAKER</p>
-        <h1 class="Title">Nike</h1>
-        <p class="Price">IDR 73.309.999</p>
-        <p class="Info">
+      </div>
+  );
+}
+
+function ProductInfo(props) {
+  const { category, title } = props;
+  return (
+    <div>
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title">{title}</h1>
+        <p className="Price">IDR 73.309.999</p>
+        <p className="Info">
           One of the most recognizable shoes in the Nike collection, The Air Unknown features lightweight, visible cushioning just like the original from '88. Signature details and materials celebrate the game-changing icon.
         </p>
       </div>  
