bc22a7ec5753368bb4dc5cedfd1a2030bb9d7bcb
muhanggaohs <angga.oktaharisetia@gmail.com>
2021-09-27 14:46:03 +0700

    react convert

---
+++ html-to-react/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "assets/css/style.css";
+import Homepage from "assets/scenes/Homepage";
+function App() {
+  return <div className="App">
+    <Homepage></Homepage>
+  </div>;
+}
+
+export default App;
